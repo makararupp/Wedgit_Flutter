8d006202ce6ad73dec0eb4ebaa424e994fe9fc1f
makaraDevIt <makarasam68@gmail.com>
2025-07-21 22:21:11 +0700

    clone_iu_app

--- lib/children_module/page/row_column_page.dart
+++ lib/children_module/page/row_column_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class RowColumnPage extends StatefulWidget {
@@ -24,69 +25,140 @@ class _RowColumnPageState extends State<RowColumnPage> {
   }
 
   Widget _buildTopMenu(){
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        TextButton(
-          onPressed: () {},
-          child: Text('Makara'),
-        ),
-        TextButton(
-          onPressed: () {},
-          child: Text('Makara'),
-        ),
-        TextButton(
-          onPressed: () {},
-          child: Text('Makara'),
-        ),
-        TextButton(
-          onPressed: () {},
-          child: Text('Makara'),
-        ),
-      ],
+    return SingleChildScrollView(
+     scrollDirection: Axis.horizontal,
+      physics: BouncingScrollPhysics(),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            onPressed: () {},
+            child: Text('Java'),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text('Oracle'),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text('IMB'),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text('Spring boot'),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text('Developer'),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text('MobileAPP'),
+          ),
+        ],
+      ),
     );
   }
 
   Widget _buildBody(){
     return Container(
       alignment: Alignment.center,
-      child: _buildRow(),
+      color: Colors.grey[300],
+      child: SingleChildScrollView(
+        child: _buildIGPost(),
+      ),
+    );
+  }
+
+  Widget _buildIGPost(){
+    String ig = "https://media.licdn.com/dms/image/v2/D5603AQEeZIJ_l6WsFQ/profile-displayphoto-shrink_800_800/B56Zcg_ESSGoAc-/0/1748605093428?e=1755734400&v=beta&t=rCK1ZUDk0u9c6St0EDdU9lT2NVMdARr5AzhXYr5Iupg";
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            children:
+             [
+               IconButton(onPressed: (){},  icon: Icon(Icons.face)),
+               Text("makara_sam7777"),
+               Spacer(),
+               IconButton(onPressed: (){}, icon: Icon(Icons.more_horiz),),
+             ],
+      
+          ),
+          Container(
+              height: 400.0,
+              width: double.maxFinite,
+              child: Image.network(ig,
+                fit: BoxFit.cover,
+              ),
+          ),
+          Row(
+            children: [
+              IconButton(onPressed: (){},
+                icon: Icon(CupertinoIcons.heart)
+              ),
+              IconButton(onPressed: (){},
+                icon: Icon(CupertinoIcons.chat_bubble),
+              ),
+              IconButton(onPressed: (){},
+                  icon: Icon(CupertinoIcons.arrow_up_circle),
+              ),
+              Spacer(),
+              IconButton(onPressed: (){},
+                icon: Icon(CupertinoIcons.bookmark),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
   
   Widget _buildRow(){
    return Container(
-     color: Colors.teal.withOpacity(0.5),
-     child: Row(
-      crossAxisAlignment: CrossAxisAlignment.end,
-         mainAxisAlignment: MainAxisAlignment.spaceAround,
-         children: [
-           Container(width: 50.0,height: 120, color: Colors.blue,),
-           Container(width: 40.0,height: 150, color: Colors.yellow,),
-           Container(width: 60.0,height: 60, color: Colors.lime,),
-           Container(width: 20.0,height: 90, color: Colors.orange,),
-           Container(width: 30.0,height: 100, color: Colors.purple,),
-           Container(width: 50.0,height: 50, color: Colors.blueGrey,),
-         ],
-     ),
+     color: Colors.blue.withOpacity(0.5),
+       child: Row(
+           children: [
+             Container(width: 50.0,height: 50.0, color: Colors.red,),
+             SizedBox(width: 10.0),
+             Container(width: 50.0,height: 50.0, color: Colors.yellow,),
+             SizedBox(width: 10.0),
+             Container(width: 50.0,height: 50.0, color: Colors.lime,),
+             Spacer(), //
+             Container(width: 50.0,height: 50.0, color: Colors.orange,),
+           ],
+       ),
    );
   }
 
   Widget _buildColumn(){
     return Container(
       color: Colors.teal.withOpacity(0.5),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(width: 150.0,height: 120, color: Colors.blue,),
-          Container(width: 150.0,height: 150, color: Colors.yellow,),
-          Container(width: 60.0,height: 60, color: Colors.lime,),
-          Container(width: 120.0,height: 90, color: Colors.orange,),
-          Container(width: 60.0,height: 100, color: Colors.purple,),
-          Container(width: 150.0,height: 50, color: Colors.blueGrey,),
-        ],
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, //  not affect.
+          children: [
+            Container(width: 150.0,height: 120, color: Colors.blue,),
+            SizedBox(height: 20.0),
+            Container(width: 150.0,height: 150, color: Colors.yellow,),
+            SizedBox(height: 20.0),
+            Container(width: 60.0,height: 60, color: Colors.lime,),
+            SizedBox(height: 20.0),
+            Container(width: 120.0,height: 90, color: Colors.orange,),
+            SizedBox(height: 20.0),
+            Container(width: 60.0,height: 100, color: Colors.purple,),
+            Container(width: 150.0,height: 50, color: Colors.blueGrey,),
+            Container(width: 150.0,height: 120, color: Colors.blue,),
+            Container(width: 150.0,height: 150, color: Colors.yellow,),
+            Container(width: 60.0,height: 60, color: Colors.lime,),
+            Container(width: 120.0,height: 90, color: Colors.orange,),
+            Container(width: 60.0,height: 100, color: Colors.purple,),
+            Container(width: 150.0,height: 50, color: Colors.blueGrey,),
+          ],
+        ),
       ),
     );
   }
-
 }
