6d900de1b15c7a60c8c501f2b10e03af5c137cdd
makaraDevIt <makarasam68@gmail.com>
2025-07-23 20:31:45 +0700

    disable_rotation_screen

--- lib/children_module/page/gridview_page.dart
+++ lib/children_module/page/gridview_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class GradViewPage extends StatefulWidget {
   const GradViewPage({super.key});
@@ -10,6 +11,12 @@ class GradViewPage extends StatefulWidget {
 class _GradViewPageState extends State<GradViewPage> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+       DeviceOrientation.portraitUp,    // Only portrait up
+       DeviceOrientation.portraitDown, // Add if you want upside-down portrait too
+      /*DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,*/
+    ]);
     return Scaffold(
       appBar: AppBar(
         title: Text("GridView Page",style: TextStyle(color:Colors.white),),
@@ -19,13 +26,16 @@ class _GradViewPageState extends State<GradViewPage> {
   }
 
   Widget _buildBody(){
+    return _buildViewCount();
+  }
+
+  Widget _buildViewCount(){
     return GridView.count(
-      padding: EdgeInsets.all(15.0),
       physics: BouncingScrollPhysics(),
       crossAxisCount: 3, // number of columns.
-      mainAxisSpacing: 10,
+      mainAxisSpacing: 5,
       crossAxisSpacing: 5,
-      childAspectRatio: 3/4, // x/y.វាប្រៀបបានដូចរូបថត។
+      childAspectRatio: 9/16, // x/y.វាប្រៀបបានដូចរូបថត។
       children: [
         Container(width: 60.0,height: 100, color: Colors.purple,),
         Container(width: 150.0,height: 50, color: Colors.blueGrey,),
@@ -38,4 +48,26 @@ class _GradViewPageState extends State<GradViewPage> {
       ],
     );
   }
+
+  Widget _buildViewExtent(){
+    return GridView.extent(
+      padding: EdgeInsets.all(15.0),
+      physics: BouncingScrollPhysics(),
+      maxCrossAxisExtent: 130.0,  // independent pixel
+      mainAxisSpacing: 10,
+      crossAxisSpacing: 5,
+      childAspectRatio: 9/16, // x/y.វាប្រៀបបានដូចរូបថត។
+      children: [
+        Container(width: 60.0,height: 100, color: Colors.purple,),
+        Container(width: 150.0,height: 50, color: Colors.blueGrey,),
+        Container(width: 150.0,height: 120, color: Colors.blue,),
+        Container(width: 150.0,height: 150, color: Colors.yellow,),
+        Container(width: 60.0,height: 60, color: Colors.lime,),
+        Container(width: 120.0,height: 90, color: Colors.orange,),
+        Container(width: 60.0,height: 100, color: Colors.red,),
+        Container(width: 150.0,height: 50, color: Colors.blueGrey,),
+      ],
+    );
+  }
+
 }
