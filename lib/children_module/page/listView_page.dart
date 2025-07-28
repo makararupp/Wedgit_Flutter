33767037d4ca85e2574f5777f7c67219b72b367f
makaraDevIt <makarasam68@gmail.com>
2025-07-28 21:39:42 +0700

    move_all_toOne_App

--- lib/children_module/page/listView_page.dart
+++ lib/children_module/page/listView_page.dart
@@ -28,9 +28,180 @@ class _ListviewPageState extends State<ListviewPage> {
     return ListView(
       children: [
         _buildStory(),
+        _buildViewBuilder(2),
+        _buildColumnList(3),
       ],
     );
   }
+
+  Widget _buildColumnList(int count){
+    return SingleChildScrollView(
+      physics: BouncingScrollPhysics(),
+      child: Column(
+        children: [
+          _buildIGPost(),
+          _buildIGPost2(),
+          _buildIGPost3(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildIGPost(){
+    String ig = "https://media.licdn.com/dms/image/v2/D5603AQEeZIJ_l6WsFQ/profile-displayphoto-shrink_800_800/B56Zcg_ESSGoAc-/0/1748605093428?e=1755734400&v=beta&t=rCK1ZUDk0u9c6St0EDdU9lT2NVMdARr5AzhXYr5Iupg";
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            children:
+            [
+              IconButton(onPressed: (){},  icon: Icon(Icons.face)),
+              Text("sam_makara7777"),
+              Spacer(),
+              IconButton(onPressed: (){}, icon: Icon(Icons.more_horiz),),
+            ],
+
+          ),
+          Container(
+            height: 400.0,
+            width: double.maxFinite,
+            child: Image.network(ig,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Row(
+            children: [
+              IconButton(onPressed: (){},
+                  icon: Icon(CupertinoIcons.heart)
+              ),
+              IconButton(onPressed: (){},
+                icon: Icon(CupertinoIcons.chat_bubble),
+              ),
+              IconButton(onPressed: (){},
+                icon: Icon(CupertinoIcons.arrow_up_circle),
+              ),
+              Spacer(),
+              IconButton(onPressed: (){},
+                icon: Icon(CupertinoIcons.bookmark),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+  Widget _buildIGPost2(){
+    String ig = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA7GhrOqUPfstlE2t4W3ij99FVYWJKY3Td3Q&s";
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            children:
+            [
+              IconButton(onPressed: (){},  icon: Icon(Icons.face)),
+              Text("Dragon_movie"),
+              Spacer(),
+              IconButton(onPressed: (){}, icon: Icon(Icons.more_horiz),),
+            ],
+
+          ),
+          Container(
+            height: 400.0,
+            width: double.maxFinite,
+            child: Image.network(ig,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Row(
+            children: [
+              IconButton(onPressed: (){},
+                  icon: Icon(CupertinoIcons.heart)
+              ),
+              IconButton(onPressed: (){},
+                icon: Icon(CupertinoIcons.chat_bubble),
+              ),
+              IconButton(onPressed: (){},
+                icon: Icon(CupertinoIcons.arrow_up_circle),
+              ),
+              Spacer(),
+              IconButton(onPressed: (){},
+                icon: Icon(CupertinoIcons.bookmark),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+  Widget _buildIGPost3(){
+    String ig = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAkKPFaTuL7DhzknMnOvk5s32984nWlPyMvg&s";
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            children:
+            [
+              IconButton(onPressed: (){},  icon: Icon(Icons.face)),
+              Text("dev_ops"),
+              Spacer(),
+              IconButton(onPressed: (){}, icon: Icon(Icons.more_horiz),),
+            ],
+
+          ),
+          Container(
+            height: 400.0,
+            width: double.maxFinite,
+            child: Image.network(ig,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Row(
+            children: [
+              IconButton(onPressed: (){},
+                  icon: Icon(CupertinoIcons.heart)
+              ),
+              IconButton(onPressed: (){},
+                icon: Icon(CupertinoIcons.chat_bubble),
+              ),
+              IconButton(onPressed: (){},
+                icon: Icon(CupertinoIcons.arrow_up_circle),
+              ),
+              Spacer(),
+              IconButton(onPressed: (){},
+                icon: Icon(CupertinoIcons.bookmark),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildViewBuilder(int count){
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount:3,
+        mainAxisSpacing: 5,
+        crossAxisSpacing: 5,
+        childAspectRatio: 1/2,
+      ),
+      itemCount: movieListConstant.length,
+      itemBuilder: (context, index){
+        return Container(
+          child: Image.network(
+            movieListConstant[index].imageUrl,
+            fit: BoxFit.cover,
+          ),
+        );
+      },
+    );
+  }
+
   Widget _buildStory(){
     return SizedBox(
       height: 250.0,
