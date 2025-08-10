03a59a4e848b69bc343b7a63fe76736f78c88980
makaraDevIt <makarasam68@gmail.com>
2025-08-10 14:47:33 +0700

    state_management_provider

---
+++ lib/state_module/apps/state_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class StateApp extends StatelessWidget {
+  const StateApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+
+      ],
+      child: MaterialApp(
+        
+      ),
+    );
+  }
+}
